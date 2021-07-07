67f13ecb205a064dba18de130192a25ea008f5f0
Maksym.Butenko <maksym.butenko@cellebrite.com>
2021-07-07 17:51:08 +0300

    Add OWIN server

---
+++ Test.Api/Test.Api/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:8000"))
+            {
+                Console.WriteLine("Server is running on http://localhost:8000 ...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Test.Api/Test.Api/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Test.Api.Startup))]
+
+namespace Test.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                return context.Response.WriteAsync("Test");
+            });
+        }
+    }
+}
